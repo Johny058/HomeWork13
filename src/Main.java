b8a5bdb5b1a50a760b676196189522e5b2b8d94d
Тимур <tuhvatullin_163@mail.ru>
2023-08-01 00:54:47 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
+        Author lev = new Author("Лев", "Толстой");
+        Author michail = new Author("Михаил", "Булгаков");
+        Book warAndPeace = new Book("Война и мир", lev, 1964);
+        Book theMasterAndMargarita = new Book("Мастер и Маргарита", michail, 1927);
+        printBook(warAndPeace);
+        printBook(theMasterAndMargarita);
+        theMasterAndMargarita.setPublishingYear(1928);
+        printBook(warAndPeace);
+        System.out.println(warAndPeace.equals(theMasterAndMargarita));
+        Book warAndPeaceNew = new Book("Война и мир", lev, 2023);
+        System.out.println(warAndPeace.equals(warAndPeaceNew));
+        System.out.println(warAndPeace.getClass());
+        System.out.println(theMasterAndMargarita.getClass());
+    }
+
+    private static void printBook(Book book) {
+        System.out.println(book.toString());
     }
 }
